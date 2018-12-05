74351c316e6d29df8ffd05e9145a426f78572220
iwalkalone <iwalkalone69@gmail.com>
2018-12-05 16:45:57 +0100

    Initial commit.

---
+++ src/iwalkalone/Logger.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace iwalkalone;
+
+class Logger
+{
+    protected $codeception = false;
+
+    public function __construct()
+    {
+        $this->codeception = class_exists('\Codeception\Util\Debug');
+    }
+
+    public function consoleLog($s_message, $color = 'green', $timestamp = true, $endline = true)
+    {
+        $start = "\033[";
+        $end = "\033[0m\n";
+        $bash_color = '0;32';
+        $colors = array(
+            'green' => '0;32',
+            'red' => '0;31',
+            'yellow' => '0;33',
+            'blue' => '0;34',
+            'purple' => '0;35',
+            'black' => '0;30',
+            'white' => '0;28',
+        );
+        if (isset($colors[$color])) {
+            $bash_color = $colors[$color];
+        }
+        if ($timestamp) {
+            $msg = $start . $bash_color . 'm' . date('H:i:s ') . $s_message;
+            if ($this->codeception) {
+                \Codeception\Util\Debug::debug($msg);
+            } else {
+                echo $msg;
+            }
+        } else {
+            $msg = $start . $bash_color . 'm' . $s_message;
+            if ($this->codeception) {
+                \Codeception\Util\Debug::debug($msg);
+            } else {
+                echo $msg;
+            }
+        }
+        if ($endline) {
+            if ($this->codeception) {
+            } else {
+                echo $end;
+            }
+        }
+    }
+}
